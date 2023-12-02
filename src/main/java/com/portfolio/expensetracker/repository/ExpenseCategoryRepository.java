322aa830408e971d77c5fab4b47acf2522f1d668
Tosif khan <khantosif94@gmail.com>
2023-12-02 22:25:21 +0530

    fixed expense category repository

--- src/main/java/com/portfolio/expensetracker/repository/ExpenseCategoryRepository.java
+++ src/main/java/com/portfolio/expensetracker/repository/ExpenseCategoryRepository.java
@@ -1,9 +1,9 @@
 package com.portfolio.expensetracker.repository;
 
-import com.portfolio.expensetracker.model.Budget;
+import com.portfolio.expensetracker.model.ExpenseCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExpenseCategoryRepository extends JpaRepository<Budget, Long> {
+public interface ExpenseCategoryRepository extends JpaRepository<ExpenseCategory, Long> {
 }
