15fa9406d63fdfded2b1852dcc34c398ac570797
Tosif khan <khantosif94@gmail.com>
2023-12-03 15:50:20 +0530

    added custom queries in the repository

--- src/main/java/com/portfolio/expensetracker/repository/BudgetRepository.java
+++ src/main/java/com/portfolio/expensetracker/repository/BudgetRepository.java
@@ -1,10 +1,12 @@
 package com.portfolio.expensetracker.repository;
 
 import com.portfolio.expensetracker.model.Budget;
+import com.portfolio.expensetracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 
 @Repository
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
+    public List<Budget> findByUserId(User user);
 }
--- src/main/java/com/portfolio/expensetracker/repository/ExpenseRepository.java
+++ src/main/java/com/portfolio/expensetracker/repository/ExpenseRepository.java
@@ -1,9 +1,15 @@
 package com.portfolio.expensetracker.repository;
 
 import com.portfolio.expensetracker.model.Expense;
+import com.portfolio.expensetracker.model.ExpenseCategory;
+import com.portfolio.expensetracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    public List<Expense> findByUser(User user);
+    public List<Expense> findByUserAndCategoryId(User user, ExpenseCategory expenseCategoryId);
 }
